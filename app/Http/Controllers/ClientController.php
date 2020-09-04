37e9e933099abe5700005169d0a4f7965100d8ff
SoftwareEngineer01 <sistemsistemaweb@gmail.com>
2020-09-04 14:17:46 -0500

    Se agregan modelos y controladores, se agrega metodo para agregar cliente y ruta

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ClientController extends ResponseController
+{
+    public function create(Request $request){
+
+        $message = null;
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'identification' => 'required|unique:clients',
+            'date_birth' => 'required|date'
+        ]);
+
+        if($validator->fails()){
+            $message = $this->sendError('Error de validación', [$validator->errors()], 422);
+        }else{
+            $client = new Client();
+            $client->name = $request->get('name');
+            $client->identification = $request->get('identification');
+            $client->date_birth = $request->get('date_birth');
+            $client->save();
+
+            $message = $this->sendResponse($client, 'Cliente creado correctamente');
+        }
+
+        return $message;
+    }
+}
