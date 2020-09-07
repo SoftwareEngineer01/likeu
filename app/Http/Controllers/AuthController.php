0c1df7d7b5db6de728edf5f0b59998d87c3260ee
SoftwareEngineer01 <sistemsistemaweb@gmail.com>
2020-09-06 23:50:42 -0500

    Modificando readme y agregando swagger

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/**
+* @OA\Server(url="http://127.0.0.1:8000")
+*/
+
 class AuthController extends Controller
 {
     /**
@@ -18,11 +22,53 @@ class AuthController extends Controller
         $this->middleware('auth:api', ['except' => ['unauthorized','login']]);
     }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
+/**
+ * Get a JWT via given credentials.
+ *
+ * @return \Illuminate\Http\JsonResponse
+ */
+
+/**
+ * @OA\Post(
+ *     path="/api/auth/login",
+ *     tags={"Login"},
+ *     @OA\Parameter(
+ *         name="email",
+ *         in="query",
+ *         description="Email - admin@admin.com",
+ *         required=true,
+ *         @OA\Schema(type="string")
+ *     ),
+ *     @OA\Parameter(
+ *         name="password",
+ *         in="query",
+ *         description="Contraseña - 123456",
+ *         required=true,
+ *         @OA\Schema(type="string")
+ *     ),
+ *     @OA\RequestBody(
+ *          @OA\MediaType(mediaType="application/form-data",
+ *         )
+ *      ),
+ *     @OA\Response(
+ *         response="200",
+ *         description="Login usuario",
+ *         @OA\JsonContent(
+ *          required={"email","password"},
+ *          @OA\Property(property="access_token", type="string", format="access_token", example="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBp"),
+ *          @OA\Property(property="token_type", type="string", format="token_type", example="bearer"),
+ *          @OA\Property(property="expires_in", type="expires_in", example="3600"),
+ *          )
+ *     ),
+ *     @OA\Response(
+ *         response="422",
+ *         description="Error de validación",
+ *     ),
+ *     security={
+ *       {"api_key": {}}
+ *     }
+ * )
+ */
     public function login(Request $request)
     {
         $credentials = $request->only(['email', 'password']);
--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,8 +6,65 @@ use App\Models\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+/**
+* @OA\Info(title="API Clientes", version="1.0")
+*
+* @OA\Server(url="http://127.0.0.1:8000")
+*/
+
 class ClientController extends ResponseController
 {
+
+/**
+ * @OA\Post(
+ *     path="/api/addClient",
+ *     tags={"Agregar cliente"},
+ *     @OA\Parameter(
+ *         name="name",
+ *         in="query",
+ *         description="Nombre del cliente",
+ *         required=true,
+ *         @OA\Schema(type="string")
+ *     ),
+ *     @OA\Parameter(
+ *         name="identification",
+ *         in="query",
+ *         description="Identificación del cliente",
+ *         required=true,
+ *         @OA\Schema(type="string")
+ *     ),
+ *      @OA\Parameter(
+ *         name="date_birth",
+ *         in="query",
+ *         description="Fecha nacimiento del cliente",
+ *         required=true,
+ *         @OA\Schema(type="string")
+ *     ),
+ *     @OA\RequestBody(
+ *          @OA\MediaType(mediaType="application/form-data",
+ *         )
+ *      ),
+ *     @OA\Response(
+ *         response="200",
+ *         description="Creación de cliente",
+ *         @OA\JsonContent(
+ *          required={"name","identification","date_birth"},
+ *          @OA\Property(property="name", type="string", format="name", example="pepe"),
+ *          @OA\Property(property="identification", type="string", format="identification", example="123456789"),
+ *          @OA\Property(property="date_birth", type="date", example="1989-08-14"),
+ *          @OA\Property(property="id", type="integer", example="1"),
+ *          )
+ *     ),
+ *     @OA\Response(
+ *         response="422",
+ *         description="Error de validación",
+ *     ),
+ *     security={
+ *       {"api_key": {}}
+ *     }
+ * )
+ */
+
     public function create(Request $request){
 
         $message = null;
